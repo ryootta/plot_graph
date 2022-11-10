60dfc913ce0c559c013f9929092cda08f5f2467a
goto <ryotagoto8@yahoo.co.jp>
2022-11-10 13:04:00 +0000

    .

--- scripts/csv_plot.py
+++ scripts/csv_plot.py
@@ -8,17 +8,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def import_data(date, time):
-    # ファイルpathの設定
+    # dateファイルまでのパス設定
     dir_path =  os.path.join(os.path.dirname(__file__), "..", "data")
     new_dir_path = os.path.join(dir_path, date)
-    # ファイルの名前の用意 
-    raw_file_name = "raw_obstacles" + time + ".csv"
-    filter_file_name = "tracked_obstacles" +  time + ".csv"
-    true_file_name = "true_obstacle" + time + ".csv"
+    # 各ファイルのパスの用意 
+    raw_file_path = os.path.join(new_dir_path, "raw_obstacles" + time + ".csv")
+    filter_file_path = os.path.join(new_dir_path, "tracked_obstacles" +  time + ".csv")
+    true_file_path = os.path.join(new_dir_path, "true_obstacle" + time + ".csv")
     # ファイルの読み取り
-    df_raw = pd.read_csv(os.path.join(new_dir_path, raw_file_name))
-    df_filter = pd.read_csv(os.path.join(new_dir_path, filter_file_name))
-    df_true = pd.read_csv(os.path.join(new_dir_path, true_file_name))
+    #try:
+    #    df_raw = pd.read_csv(raw_file_path)
+    #    df_filter = pd.read_csv(filter_file_path)
+    #    df_true = pd.read_csv(true_file_path)
+    #except Exception as err:
+    #    print("obstacles number is more than 2.")
+    #    df_raw = pd.read_csv(raw_file_path, na_values=["no_value", 0, "missing"])
+    #    df_filter = pd.read_csv(filter_file_path, na_values=["no_value", 0, "missing"])
+    #    df_true = pd.read_csv(true_file_path, na_values=["no_value", 0, "missing"])
+    
+    df_raw = pd.read_csv(raw_file_path, na_values=["no_value", 0, "missing"])
+    df_filter = pd.read_csv(filter_file_path, na_values=["no_value", 0, "missing"])
+    df_true = pd.read_csv(true_file_path, na_values=["no_value", 0, "missing"])
+
     print(df_raw)
     # pandasのdfからnumpyのndarray型に変換
     raw_array = df_raw.to_numpy()
@@ -39,20 +50,35 @@ def print_graph(array, date, time):
             raise
 
     # raw_data.jpgの作成
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 5))
     fig.suptitle('Raw Data', fontsize=16)
     raw_time = [i/40 for i in range(len(array[0]))]
     true_time = [i/1000 for i in range(len(array[2]))]
-    axs[0].scatter(raw_time,  array[0][:, 1], s=1, label="x")
-    axs[0].scatter(true_time, array[2][:, 0], s=1, label="tx")
-    axs[1].scatter(raw_time,  array[0][:, 2], s=1, label="y")
-    axs[1].scatter(true_time, array[2][:, 1], s=1, label="ty")
-    axs[0].set_ylabel("x")
-    axs[0].set_xlabel("Time [s]")
-    axs[1].set_ylabel("y")
-    axs[1].set_xlabel("Time [s]")
+    axs[0][0].scatter(raw_time,  array[0][:, 1], s=1, label="x")
+    axs[0][0].scatter(true_time, array[2][:, 0], s=1, label="tx")
+    axs[0][1].scatter(raw_time,  array[0][:, 2], s=1, label="y")
+    axs[0][1].scatter(true_time, array[2][:, 1], s=1, label="ty")
+    axs[1][0].scatter(raw_time,  array[0][:, 1+5], s=1, label="x")
+    axs[1][0].scatter(true_time, array[2][:, 0], s=1, label="tx")
+    axs[1][1].scatter(raw_time,  array[0][:, 2+5], s=1, label="y")
+    axs[1][1].scatter(true_time, array[2][:, 1], s=1, label="ty")
+    axs[0][0].set_ylabel("x")
+    axs[0][1].set_ylabel("y")
+    axs[1][0].set_ylabel("x")
+    axs[1][0].set_xlabel("Time [s]")
+    axs[1][1].set_ylabel("y")
+    axs[1][1].set_xlabel("Time [s]")
     #axs[0].set_ylim(ylimit[0], ylimit[1])
-    plt.savefig(os.path.join("figure", "raw_data" + time + ".jpg"))
+    axs[0][0].set_xlim(0, 20)
+    axs[0][1].set_xlim(0, 20)
+    axs[1][0].set_xlim(0, 20)
+    axs[1][1].set_xlim(0, 20)
+    axs[0][0].set_ylim(0, 2.2)
+    axs[0][1].set_ylim(-2.2, 2.2)
+    axs[1][0].set_ylim(0, 2.2)
+    axs[1][1].set_ylim(-2.2, 2.2)
+
+    plt.savefig(os.path.join(dir_path, "raw_data" + time + ".jpg"))
 
     # tracked_ata.jpgの作成
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 6))
@@ -68,11 +94,16 @@ def print_graph(array, date, time):
     axs[1][1].set_ylabel("vy")
 
     #plt.show()
-    plt.savefig(os.path.join("figure", "tracked_data" + time + ".jpg"))
+    plt.savefig(os.path.join(dir_path, "tracked_data" + time + ".jpg"))
 
 
-date = "2022-11-08"
-time = "-12-17"
+#date = "2022-11-08"
+#time = "-12-17"
+
+date = "2022-11-10"
+#time = "-11-20"
+time = "-11-16"
+
 array = import_data(date, time)
 print(array[0])
 print_graph(array, date, time)
